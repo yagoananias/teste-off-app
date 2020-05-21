421b753c38c50e81ae7da8ccf48d035e1e44baea
Yago Ananias <yago_souza92@yahoo.com.br>
2020-05-20 22:09:08 -0300

    Refatorando Componentes

--- app/index.js
+++ app/index.js
@@ -2,30 +2,23 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
 
 const { width, height } = Dimensions.get('window');
+
 class MusicApp extends Component {
+
   render() {
     return (
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: 'white',
-          justifyContent: 'flex-end'
-        }}
-      >
+      <View style={styles.body}>
         <View style={{ ...StyleSheet.absoluteFill }}>
-          <Image
-            source={require('../assets/bg.jpg')}
-            style={{ flex: 1, height: null, width: null }}
-          />
+          <Image source={require('../assets/bg.jpg')}style={styles.image}/>
         </View>
+
         <View style={{ height: height / 3, justifyContent: 'center' }}>
-          <View style={styles.button}>
-            <Text style={{ fontSize: 20, fontWeight: 'bold' }}>SIGN IN</Text>
+          <View style={styles.buttonEntrar}>
+            <Text style={styles.entrarTexto}>ENTRAR</Text>
           </View>
-          <View style={{ ...styles.button, backgroundColor: '#2E71DC' }}>
-            <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'white' }}>
-              SIGN IN WITH FACEBOOK
-            </Text>
+
+          <View style={styles.buttonFace}>
+            <Text style={styles.facebook}>ENTRE COM O FACEBOOK</Text>
           </View>
         </View>
       </View>
@@ -40,13 +33,41 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center'
   },
-  button: {
+  buttonEntrar: {
     backgroundColor: 'white',
-    height: 70,
+    height: 50,
     marginHorizontal: 20,
     borderRadius: 35,
     alignItems: 'center',
     justifyContent: 'center',
     marginVertical: 5
+  },
+  buttonFace: {
+    backgroundColor: '#2E71DC',
+    height: 50,
+    marginHorizontal: 20,
+    borderRadius: 35,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 5
+  },
+  body: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'flex-end'
+  },
+  image: {
+    flex: 1, 
+    height: null,
+    width: null
+  },
+  facebook: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: 'white'
+  },
+  entrarTexto: {
+    fontSize: 15,
+    fontWeight: 'bold'
   }
 });
